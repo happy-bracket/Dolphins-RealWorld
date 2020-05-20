315c98ff453eeddc52a13c5903fb783b6a7b01b9
Egor <yytruten@mts.ru>
2020-05-20 23:12:06 +0300

    idk

--- app/src/main/java/ru/hbracket/realworld/App.kt
+++ app/src/main/java/ru/hbracket/realworld/App.kt
@@ -2,11 +2,14 @@ package ru.hbracket.realworld
 
 import android.app.Application
 import android.content.Context
+import com.facebook.soloader.SoLoader
 
 class App : Application() {
 
     override fun onCreate() {
         super.onCreate()
+
+        SoLoader.init(this, false)
 
         instance = this
     }
--- app/src/main/java/ru/hbracket/realworld/MainActivity.kt
+++ app/src/main/java/ru/hbracket/realworld/MainActivity.kt
@@ -2,16 +2,22 @@ package ru.hbracket.realworld
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
+import com.facebook.litho.ComponentContext
+import com.facebook.litho.LithoView
+import com.facebook.litho.widget.Image
+import com.facebook.litho.widget.Text
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
         supportActionBar?.title = "Conduit"
 
+        val ctx = ComponentContext(this)
+        val c = Text.create(ctx).text("jopa").build()
 
 
+        setContentView(LithoView.create(ctx, c))
     }
 }
